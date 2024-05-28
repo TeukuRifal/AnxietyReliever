9da681e31b8e5b24082ff0b1f1b4b8748150d3fc
rifal-droid <trifalaulia@gmail.com>
2024-05-28 19:22:38 +0700

    update projek

--- start/routes.ts
+++ start/routes.ts
@@ -5,20 +5,9 @@ const AccountsController = () => import('#controllers/accounts_controller')
 const PagesController = () => import('#controllers/pages_controller')
 const UsersController = () => import('#controllers/users_controller')
 
-// // router.group(() => {
-  router.get('/beranda', [PagesController, 'beranda'])
-//   router.get('/', [PagesController, 'beranda'])
-//   router.post('/logout', [AccountsController, 'logout'])
-//   router.get('/crud', [PagesController, 'crud'])
-//   router.get('/about', [PagesController, 'about'])
-// // }).use(middleware.auth())
 
+router.get('/beranda', [PagesController, 'beranda'])
 
-// router.post('/login', [AccountsController, 'login'])
-// router.get('/login', [AccountsController, 'index'])
-
-// router.resource('account', AccountsController)
-// router.resource('user', UsersController)
 
 
 router.get('/', [PagesController, 'beranda'])
@@ -26,5 +15,8 @@ router.get('/about', [PagesController, 'about'])
 router.get('/quiz', [PagesController, 'quiz'])
 router.get('/question', [PagesController, 'question'])
 router.get('/crud', [PagesController, 'crud'])
+
+
+
 
 // router.get('/register', [AccountsController, 'create'])
